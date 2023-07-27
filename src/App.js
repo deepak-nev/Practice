19664da2de6715c56ee9f0c629e59bd95b8c01be
Deepak <deepak977140@gmail.com>
2023-07-27 10:00:04 +0530

    useRef

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{createRef} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.nameRef=createRef();
+  }
+  componentDidMount(){
+   // console.warn(this.inputRef.current.value="100")
+  }
+  getChange(){
+    console.warn(this.nameRef.current.value)
+    this.nameRef.current.style.color="red"
+    this.nameRef.current.style.backgroundColor="black"
+    
+  }
+
+  render(){
+    return(
+      <div>
+      <h2>Use Ref hooks</h2>
+      <input type="text" placeholder='Enter your name' ref={this.nameRef}></input>
+      <button onClick={()=>this.getChange()}>Click me</button>
+      </div>
+    )
+    
+  }
+  
 }
 
-export default App;
+export default App
